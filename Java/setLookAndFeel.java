a350a81c9a3133e968a7c2c9c41fb091830081eb
Alde <iObsidian@users.noreply.github.com>
2017-12-27 10:45:27 -0500

    Create setLookAndFeel.java

---
+++ Java/setLookAndFeel.java
@@ -0,0 +1,6 @@
+try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
