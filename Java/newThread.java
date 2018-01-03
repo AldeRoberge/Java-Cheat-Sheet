abcdf48d2ce7c69ad3304cd88a767f17ddb5c467
Alde <iObsidian@users.noreply.github.com>
2018-01-03 12:51:42 -0500

    Update newThread.java

--- Java/newThread.java
+++ Java/newThread.java
@@ -1,5 +1,5 @@
 /**
-* If you want to stop a thread from hanging, use this trick
+* If you want to stop a program from hanging, use this trick (breaks synchronisation, the code following this will be running at the same time)
 */
 
 new Thread("Thread name") {
